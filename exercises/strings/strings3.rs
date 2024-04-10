45ec0be90765a36902e3abc95e50c0ef61be91df
空白 <103114267+getKongBai@users.noreply.github.com>
2024-04-10 14:28:02 +0000

    1

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,31 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::ops::Add;
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut s = input.to_string();
+    let mut i = 0;
+    while s.chars().collect::<Vec<char>>()[i] == ' ' {
+        s.remove(i);
+    }
+    i = s.len() - 1;
+    while s.chars().collect::<Vec<char>>()[i]  == ' ' {
+        s.remove(i);
+        i -= 1;
+    }
+    s
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string().add(" world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars","balloons")
 }
 
 #[cfg(test)]
