d1cb9a90e38f9e694e53a6ab393cfe968f466fc6
空白 <103114267+getKongBai@users.noreply.github.com>
2024-04-11 04:44:19 +0000

    1

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,32 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+pub struct R {
+    pub grade: String,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+trait Print {
+    fn print(&self) -> String;
+}
+
+impl Print for ReportCard {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+                &self.student_name, &self.student_age, &self.grade)
+    }
+}
+impl Print for R {
+    fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+                &self.student_name, &self.student_age, &self.grade)
     }
 }
 
@@ -51,8 +65,8 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card = R {
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
